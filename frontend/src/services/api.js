3a92aa5ab64f7062ec0a80e77300ed0b68ae7f7a
KANISHK PATIL <25patilkp@rbunagpur.in>
2026-08-07 23:19:19 +0530

    Update API base URL to include '/api/'

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL: "https://recruitment-assessment-main-ksuxu7fu1-kanishk17.vercel.app/"
+    baseURL: "https://recruitment-assessment-main-ksuxu7fu1-kanishk17.vercel.app/api/"
 
 });
 
